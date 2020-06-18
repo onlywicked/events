a6d44433001f930aeaa04eace30af5c164adc707
Arman <hello@arman.work>
2020-06-18 22:00:23 +0530

    feat: add global event emitter

--- events.go
+++ events.go
@@ -1,6 +1,12 @@
 // Package events provide an in-memory implementation of event emitter pattern
 package events
 
+func init() {
+	defaultEmitter = NewEventEmitter()
+}
+
+var defaultEmitter EventEmitter
+
 // Listener is function that listens on an event
 type Listener func(data Data)
 
@@ -24,4 +30,25 @@ type EventEmitter interface {
 	On(event string, l Listener)
 	Close()
 	OnAll(l Listener)
+}
+
+// List returns all the events listeners are listening to
+func List() []string {
+	return defaultEmitter.List()
+}
+
+// Emit emits an event to all the listeners listening
+func Emit(event string, data Data) {
+	defaultEmitter.Emit(event, data)
+}
+
+// On attaches a listener to an event
+func On(event string, l Listener) {
+	defaultEmitter.On(event, l)
+}
+
+// OnAll attaches a listener to all events
+// In simpler terms, attaches a global listener
+func OnAll(l Listener) {
+	defaultEmitter.OnAll(l)
 }
