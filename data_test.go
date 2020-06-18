45f2aa6ae78ec1460c9fa9529bb89a1b29ec8d03
Arman <hello@arman.work>
2020-06-18 20:45:19 +0530

    test: for json unmarshal interface

---
+++ data_test.go
@@ -0,0 +1,61 @@
+package events
+
+import (
+	"encoding/json"
+	"errors"
+	"testing"
+)
+
+func TestData_MarshalJSON(t *testing.T) {
+	type fields struct {
+		Payload interface{}
+		Message string
+		Error   error
+		event   string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   map[string]interface{}
+	}{
+		{
+			name: "should return only specified field",
+			fields: fields{
+				event:   "event:happened",
+				Payload: "some payload",
+			},
+			want: map[string]interface{}{
+				"event":   "event:happened",
+				"payload": "some payload",
+			},
+		},
+		{
+			name: "should use error string",
+			fields: fields{
+				event: "event:happened",
+				Error: errors.New("error"),
+			},
+			want: map[string]interface{}{
+				"event": "event:happened",
+				"error": "error",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := Data{
+				Payload: tt.fields.Payload,
+				Message: tt.fields.Message,
+				Error:   tt.fields.Error,
+				event:   tt.fields.event,
+			}
+			got, _ := e.MarshalJSON()
+			want, _ := json.Marshal(tt.want)
+
+			if string(got) != string(want) {
+				t.Errorf("Data.MarshalJSON(): expected = %v, got = %v", string(want), string(got))
+			}
+		})
+	}
+}
