7b2ca9a56f5de826f182adc5b81392467e93fff6
Arman <hello@arman.work>
2020-06-18 20:45:50 +0530

    feat: add event data for passing payloads

---
+++ data.go
@@ -0,0 +1,39 @@
+package events
+
+import "encoding/json"
+
+// Data represents an event data in a event lifecycle
+type Data struct {
+	event   string
+	Payload interface{}
+	// Optional
+	Message string
+	Error   error
+}
+
+// String implements the stringer interface for the Data
+func (e Data) String() string {
+	j, _ := e.MarshalJSON()
+	return string(j)
+}
+
+// MarshalJSON implements the json marshaler interface for the Data
+func (e Data) MarshalJSON() ([]byte, error) {
+	eM := make(map[string]interface{})
+
+	eM["event"] = e.event
+
+	if e.Payload != nil {
+		eM["payload"] = e.Payload
+	}
+
+	if e.Message != "" {
+		eM["message"] = e.Message
+	}
+
+	if e.Error != nil {
+		eM["error"] = e.Error.Error()
+	}
+
+	return json.Marshal(eM)
+}
